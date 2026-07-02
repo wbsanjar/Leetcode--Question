fef5775f333ee9b8d4eb95b8ec6d3075cba42424
Md Sanjar <codemonu77@gmail.com>
2026-07-02 22:38:09 +0530

    Time: 4 ms (84.68%), Space: 46.4 MB (71.44%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+
+        return max;
+    }
+}
