7177f5313d0346c045a2af0f1a457a05f57c7f47
Md Sanjar <codemonu77@gmail.com>
2026-07-27 22:42:49 +0530

    Time: 1 ms (96.03%), Space: 41.9 MB (86.68%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    int count = 0;
+
+    public int totalNQueens(int n) {
+
+        boolean[] col = new boolean[n];
+        boolean[] diag1 = new boolean[2 * n - 1];
+        boolean[] diag2 = new boolean[2 * n - 1];
+
+        solve(0, n, col, diag1, diag2);
+
+        return count;
+    }
+
+    private void solve(int row, int n, boolean[] col,
+                       boolean[] diag1, boolean[] diag2) {
+
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int c = 0; c < n; c++) {
+
+            int d1 = row - c + n - 1;
+            int d2 = row + c;
+
+            if (col[c] || diag1[d1] || diag2[d2])
+                continue;
+
+            col[c] = true;
+            diag1[d1] = true;
+            diag2[d2] = true;
+
+            solve(row + 1, n, col, diag1, diag2);
+
+            col[c] = false;
+            diag1[d1] = false;
+            diag2[d2] = false;
+        }
+    }
+}
